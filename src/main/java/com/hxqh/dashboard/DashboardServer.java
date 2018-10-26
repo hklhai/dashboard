bc1ca26e571ad786c8781ba820fbb81c984410bc
hklhai <hkhai@outlook.com>
2018-10-26 17:07:12 +0800

    修正jackson反序列化引起hibernate级联查询问题

--- src/main/java/com/hxqh/dashboard/DashboardServer.java
+++ src/main/java/com/hxqh/dashboard/DashboardServer.java
@@ -1,12 +1,16 @@
 package com.hxqh.dashboard;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -27,6 +31,16 @@ public class DashboardServer extends SpringBootServletInitializer implements Emb
     public void customize(ConfigurableEmbeddedServletContainer container) {
         container.setPort(8090);
     }
+
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = jsonConverter.getObjectMapper();
+        objectMapper.registerModule(new Hibernate5Module());
+        return jsonConverter;
+    }
+
 }
 
 
