598f4418a59860c46687949495aa47be63ac6cdd
hklhai <hkhai@outlook.com>
2018-11-07 14:32:29 +0800

    remove test

--- src/test/java/com/hxqh/dashboard/test/RandomTest.java
+++ src/test/java/com/hxqh/dashboard/test/RandomTest.java
@@ -10,14 +10,13 @@ import java.util.Random;
  * @author Ocean lin
  */
 public class RandomTest {
-
-    @Test
-    public void testRandom() {
-        for (int i = 1; i < 9; i++) {
-            Random random = new Random();
-            int rand = random.nextInt(60) + 10;
-            System.out.println(rand);
-        }
-    }
+//    @Test
+//    public void testRandom() {
+//        for (int i = 1; i < 9; i++) {
+//            Random random = new Random();
+//            int rand = random.nextInt(60) + 10;
+//            System.out.println(rand);
+//        }
+//    }
 
 }
