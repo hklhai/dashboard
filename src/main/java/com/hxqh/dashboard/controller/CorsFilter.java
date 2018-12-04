99ff298ad6237dc27b19287cc4e3a9de4a8d68a8
hklhai <hkhai@outlook.com>
2018-12-04 09:42:17 +0800

    修改返回信息

--- src/main/java/com/hxqh/dashboard/controller/CorsFilter.java
+++ src/main/java/com/hxqh/dashboard/controller/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE,OPTIONS");
 
         chain.doFilter(req, res);
     }
