5270db7b5421890f745ebd084651f8874382da49
hklhai <hkhai@outlook.com>
2018-12-05 09:03:21 +0800

    修正跨域问题

--- src/main/java/com/hxqh/dashboard/controller/CorsFilter.java
+++ src/main/java/com/hxqh/dashboard/controller/CorsFilter.java
@@ -3,6 +3,7 @@ package com.hxqh.dashboard.controller;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -24,9 +25,13 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE,OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Max-Age", "1728000");
 
         chain.doFilter(req, res);
     }
+
 
     @Override
     public void init(FilterConfig filterConfig) {
